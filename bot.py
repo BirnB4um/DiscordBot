3800024e869929776faf2bcd962b8106746814f3
Thimo Thorandt <87324856+BirnB4um@users.noreply.github.com>
2023-10-09 19:15:49 +0200

    fixed bug when calling picoGPT

--- bot.py
+++ bot.py
@@ -151,7 +151,7 @@ async def on_message(message):
         #if not a command
         if message.content != ".":
             input = f"{message.author.name} is talking to Furby.\n{message.author.name}: {message.content}\nFurby: "
-            output = await bot.loop.run_in_executor(None, picoBot.run(input, 10))
+            output = await bot.loop.run_in_executor(None, picoBot.run, input, 10)
             log(type(output))
             log(output)
             output = output.split(f"{message.author.name}:")[0]
@@ -227,7 +227,7 @@ async def talk(ctx, *message):
         return
 
     input = f"{ctx.author.name} is talking to Furby.\n{ctx.author.name}: {msg}\nFurby: "
-    output = await bot.loop.run_in_executor(None, picoBot.run(input, 10))
+    output = await bot.loop.run_in_executor(None, picoBot.run, input, 10)
     log(type(output))
     log(output)
     output = output.split(f"{ctx.author.name}:")[0]
