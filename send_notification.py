7a736d776472205dc17ce55a6e49270420aae8a8
Thimo Thorandt <87324856+BirnB4um@users.noreply.github.com>
2024-03-15 11:31:52 +0100

    Add send_notification for log notifications

---
+++ send_notification.py
@@ -0,0 +1,27 @@
+
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+import os
+
+def send_notification(message, file_path=None):
+    if file_path and not os.path.exists(file_path):
+        file_path = None
+
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    intents = discord.Intents.all()
+    intents.members = True
+    bot = commands.Bot(command_prefix='.', intents=intents)
+
+    @bot.event
+    async def on_message(message):
+        return
+
+    @bot.event
+    async def on_ready():
+        user = await bot.fetch_user(618140491879546881)
+        await user.send(message, file=discord.File(file_path) if file_path else None)
+        await bot.close()
+
+    bot.run(TOKEN)
